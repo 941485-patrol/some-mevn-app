1f2ec4131ba84727884d16a07619e9a5a4c7111a
941485-patrol <dribbleroar@gmail.com>
2020-09-08 20:19:19 +0800

    refactor status unit tests

--- server/app/routes/status/sortStatuses.js
+++ server/app/routes/status/sortStatuses.js
@@ -1,6 +1,6 @@
 const sortStatuses = function(req) {
     var sort = req.query.sort == undefined ? '_id' : req.query.sort.toString();
-    var sortFields = ['_id', 'name', 'description', 'animal_id', '-_id', '-name', '-description', '-animal_id']; 
+    var sortFields = ['_id', 'name', 'description', '-_id', '-name', '-description']; 
     if (sortFields.includes(sort) == false) throw new Error('Invalid sort field.');
     return sort;
 }
